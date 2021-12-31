140de5ff7fa2a04ed33b5f13df56f73a24441605
jiangWeixian <jiangweixian1994@gmail.com>
2021-12-31 16:54:02 +0800

    :sparkles:  list exported function

--- src/register.js
+++ src/register.js
@@ -1,5 +1,7 @@
 const addHook = require('pirates').addHook
 const pc = require('picocolors')
+const fs = require('fs')
+const { parse } = require('es-module-lexer')
 
 const argv = require('yargs-parser')(process.argv.slice(2), { array: 'p' })
 
@@ -7,8 +9,23 @@ const scriptFilename = process.argv[1]
 const funcName = argv._[0]
 const params = argv.p
 
+const showFunctionList = () => {
+  parse(fs.readFileSync(scriptFilename).toString()).then((res) => {
+    const [, exports] = res
+    console.log(pc.cyan('Exported function will displayed here:'))
+    console.log()
+    exports.forEach((element) => {
+      console.log(`- ${pc.bold(element)}`)
+    })
+  })
+}
+
 addHook(
   (code) => {
+    if (argv.help) {
+      showFunctionList()
+      return code
+    }
     if (!funcName) {
       console.error(`Ops, Try run ${pc.green('esrua [filepath] [function]')}`)
       return code
