574187ceebb13453f33f118d6c67779a808d7b2a
jiangWeixian <jiangweixian1994@gmail.com>
2022-01-17 10:21:18 +0800

    :mute:  rm debug console

--- src/index.mjs
+++ src/index.mjs
@@ -6,7 +6,5 @@ const spawnSync = spawn.sync
 
 const argv = process.argv.slice(2)
 resolve('./loader.mjs', import.meta.url).then((path) => {
-  // TODO: remove debug console in major version
-  console.log(path)
   process.exit(spawnSync('node', ['--loader', path, ...argv], { stdio: 'inherit' }).status)
 })
